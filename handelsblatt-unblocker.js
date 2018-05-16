5fc4d71d051db2b946785f7f42ab087c50dbb4ad
Twometer <twometer@outlook.de>
2018-05-16 18:45:22 +0200

    Added Handelsblatt unblocker

---
+++ handelsblatt-unblocker.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Handelsblatt AdBlock unblocker
+// @version      0.1
+// @description  Handelsblatt AdBlock unblocker
+// @author       Twometer
+// @match        http://www.handelsblatt.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.AdController = {}; // OMFG are you serious guys xD
+})();
